7bcf0ccf90eca2f067fd86bdd5d289ebc840baac
Ahmed Mosharafa <ahmedmosharafa@gmail.com>
2015-01-25 16:45:29 -0400

    el 7alla delwa2tyy

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,5 @@
 class Transaction < ActiveRecord::Base
   has_many :account, :dependent => :destroy
-  belongs_to :user attr_accessible :from_account, :to_account, :amount, :description
+  belongs_to :user 
+  attr_accessible :from_account, :to_account, :amount, :description
 end
