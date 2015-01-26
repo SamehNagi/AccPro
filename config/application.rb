9d498a6a0dda420ea0db2f0375e1d95ddd374c88
Sameh Nagi <eng.same7.la@gmail.com>
2015-01-26 18:02:03 -0400

    Fixing a bug about telling rails not to bootstrap your application during asset compilation

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-config.assets.initialize_on_precompile = false
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -37,7 +36,7 @@ module AccPro
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
-
+  
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
@@ -60,5 +59,7 @@ module AccPro
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.initialize_on_precompile = false
   end
 end
